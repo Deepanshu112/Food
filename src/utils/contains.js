ac0ac1be1ff2ff0bbf27c0461f15040cdf14f82a
Deepanshu112 <140751260+Deepanshu112@users.noreply.github.com>
2024-07-20 22:16:16 +0530

    Delete src directory

--- src/utils/contains.js
+++
@@ -1,3 +0,0 @@
-export const LOGO_URL = "https://i.pinimg.com/originals/82/be/d4/82bed479344270067e3d2171379949b3.png";
-
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
